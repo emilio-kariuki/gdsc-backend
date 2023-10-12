1ef1c5f6518c67ddede79c8cefe707069ba1b2db
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-10-12 22:05:34 +0300

    add(env): added the environment variables and the cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from 'cors';
 import UserRouter from "./routes/user/user";
 import EventRouter from "./routes/event/event";
 import GroupRouter from "./routes/group/group";
@@ -9,19 +10,32 @@ import FeedbackRouter from "./routes/help/feedback";
 import ResourceRouter from "./routes/resources/resources";
 import NotificationRouter from "./routes/notifications/notifications";
 import AuthRouter from "./routes/authentication/auth";
-import { eventJob } from "./cron_jobs/crons";
-
+// import { eventJob, completeEvent } from "./cron_jobs/crons";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
 
+const port = 4000;
+
+process.env.PORT = port.toString();
+
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+    })
+  );
 
-app.all("/", (req, res) => {
+app.all("/", (_req, res) => {
   res.send("Hello World!");
 });
 
 // eventJob.start();
+// completeEvent.start();
 
 app.use("/user", UserRouter);
 app.use("/event", EventRouter);
@@ -33,6 +47,6 @@ app.use("/resource", ResourceRouter);
 app.use("/notification", NotificationRouter);
 app.use("/auth", AuthRouter);
 
-app.listen(3000, () => {
-  console.log("Server on port 3000");
+app.listen(port, () => {
+  console.log("Server on port", port);
 });
