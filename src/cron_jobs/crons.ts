3a608c7fb38366cb2abd8fafe34781046c89b5d0
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-10-12 11:55:09 +0300

    add: added the complete event cron job

--- src/cron_jobs/crons.ts
+++ src/cron_jobs/crons.ts
@@ -14,7 +14,7 @@ export const eventJob = new CronJob(
       const title = event.name;
       const body = event.description;
       const minutes = await eventMinutes(event);
-      if ( minutes === 1 || minutes === 60) {
+      if (minutes === 1 || minutes === 60) {
         try {
           const message = {
             notification: {
@@ -25,11 +25,10 @@ export const eventJob = new CronJob(
           const response = await firebase
             .messaging()
             .sendToTopic("tedting_cron", message);
-      
+
           console.log("Successfully sent message:" + response);
         } catch (e) {
           console.log("error", e);
-          
         }
       }
     }
@@ -39,27 +38,48 @@ export const eventJob = new CronJob(
   "America/Los_Angeles"
 );
 
+export const completeEvent = new CronJob("1 * * * * *", async function () {
+  const events = await prisma.event.findMany({
+    where: {
+      isCompleted: false,
+    },
+  });
+  for (const event of events) {
+    const id = event.id;
+    const minutes = await eventMinutes(event);
+    if (minutes === -119) {
+      try {
+        const eventUpdate = await prisma.event.update({
+          where: {
+            id: id,
+          },
+          data: {
+            isCompleted: true,
+          },
+        });
 
-export const completeEvent = new CronJob(
-    "*/10 * * * * *",
-    async function () {
-
+        !eventUpdate
+          ? console.log("event failed to complete")
+          : console.log("event completed successfully");
+      } catch (e) {
+        console.log("error", e);
+      }
     }
-)
+  }
+});
 
 async function eventMinutes(event: any) {
-    const date = new Date(event.date);
+  const date = new Date(event.date);
 
-    const currentTime = new Date();
-    currentTime.setHours(currentTime.getHours());
+  const currentTime = new Date();
+  currentTime.setHours(currentTime.getHours());
 
-    const eventTimeTimestamp = date.getTime();
-    const currentTimeTimestamp = currentTime.getTime();
+  const eventTimeTimestamp = date.getTime();
+  const currentTimeTimestamp = currentTime.getTime();
 
-    const difference = eventTimeTimestamp - currentTimeTimestamp;
+  const difference = eventTimeTimestamp - currentTimeTimestamp;
 
-    const minutes = Math.ceil(difference / (1000 * 60));
-    console.log("the event is in " + minutes + " minutes");
-    return minutes;
+  const minutes = Math.ceil(difference / (1000 * 60));
+  console.log("the event is in " + minutes + " minutes");
+  return minutes;
 }
-   
