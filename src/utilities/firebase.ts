f03bd4d8b947ad885a615cd720bcc5fe69f09b54
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-10-17 23:10:10 +0300

    fix(firebase): fixed the firebase application connector and app initialization

--- src/utilities/firebase.ts
+++ src/utilities/firebase.ts
@@ -1,10 +1,11 @@
-// import admin from "firebase-admin";
-// import  serviceAccount from "../server.json";
+import admin from "firebase-admin";
+//@ts-ignore
+import  serviceAccount from "../server.json" assert { type: "json" };
 
-// const firebaseConfig: admin.AppOptions = {
-//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-//   databaseURL: "https://apt-rite-346310-default-rtdb.firebaseio.com"
-// };
+const firebaseConfig: admin.AppOptions = {
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: "https://apt-rite-346310-default-rtdb.firebaseio.com"
+};
 
-// export const firebase = admin.initializeApp(firebaseConfig);
+export const firebase = admin.initializeApp(firebaseConfig);
 
